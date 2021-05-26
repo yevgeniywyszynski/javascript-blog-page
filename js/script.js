65da25edac7a76e8d0054d5a12e39ab5b4f45308
YevgeniyWyszynski <ywyszynski@gmail.com>
2021-05-26 21:48:25 +0200

    Add remove active article

--- js/script.js
+++ js/script.js
@@ -3,14 +3,24 @@
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
   });*/
-  const titleClickHandler = function(event){
+    const titleClickHandler = function(event){
     console.log(event)
 
     /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
 
     /* add class 'active' to the clicked link */
 
     /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.post');
+
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+    }
 
     /* get 'href' attribute from the clicked link */
 

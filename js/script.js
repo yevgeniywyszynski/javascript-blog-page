47cfc081b8fd2244d3e025839224fdf86f22a9d7
YevgeniyWyszynski <ywyszynski@gmail.com>
2021-05-26 21:24:44 +0200

    "Add button"

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 'use strict';
+document.getElementById('test-button').addEventListener('click', function(){
+    const links = document.querySelectorAll('.titles a');
+    console.log('links:', links);
+  });
